debaef449065420930405dba7a79866fe0081ade
Pratik Bambulkar <72552513+Pratik180198@users.noreply.github.com>
2022-03-31 15:50:01 +0530

    deploying to heroku

--- database.py
+++ database.py
@@ -2,10 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/demo_fastapi"
+# DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/demo_fastapi"
+# DATABASE_URL = "mysql+mysqlconnector://uj2e9efqtud9o1kj:RgQUFdSt8kwpoS0hOQ1o@bbkpqybvi5txep4qrdcb-mysql.services.clever-cloud.com:3306/bbkpqybvi5txep4qrdcb"
+# engine = create_engine(DATABASE_URL)  # creating engine
+SQLALCHAMY_DB_URL = 'mysql+mysqlconnector://uvayh5ymlkx8ybfd:HJEYCKFKknzzlP7cvlKL@bjylablqdhnmt0gvpyai-mysql.services.clever-cloud.com:3306/bjylablqdhnmt0gvpyai'
 
-engine = create_engine(DATABASE_URL)  # creating engine
-
+engine = create_engine(SQLALCHAMY_DB_URL)
 Base = declarative_base()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
